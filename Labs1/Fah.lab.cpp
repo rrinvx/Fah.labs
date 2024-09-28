616e1c845f5ff4a8b5229ba5c985eed6e42063fd
Lytvynenko Regina <rina268183@gmail.com>
2024-09-28 21:16:18 +0300

    Updated gitignore

---
+++ Labs1/Fah.lab.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+
+{
+	double a, b, P, S;
+
+	cout << "Input a: ";
+	cin >> a;
+	cout << "Input b: ";
+	cin >> b;
+	S = a * b;
+	P = 2 * (a + b);
+	cout << "S= " << S << endl;
+	cout << "P= " << P << endl;
+
+	cout << "S + P = " << S + P << endl;
+	cout << "S - P = " << S - P << endl;
+	cout << "S * P = " << S * P << endl;
+	cout << "S / P = " << S / P << endl;
+	system("pause>>void");
+	return 0;
+}
